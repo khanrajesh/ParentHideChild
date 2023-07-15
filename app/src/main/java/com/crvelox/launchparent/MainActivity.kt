1a4274424b8fdd90c8b92bfd85df1ec37b64292d
khanrajesh <rajesh.k@iserveu.co.in>
2023-07-15 16:02:47 +0530

    clean up

--- app/src/main/java/com/crvelox/launchparent/MainActivity.kt
+++ app/src/main/java/com/crvelox/launchparent/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.crvelox.launchparent
 
-import android.content.ActivityNotFoundException
-import android.content.ComponentName
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,11 +9,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.crvelox.launchparent.ui.theme.LaunchParentTheme
+import com.velox.lazeir.utils.outlet.startOuterActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,48 +21,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LaunchParentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
                     Box(
                         Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                     ) {
-
                         Button(onClick = {
-                            val pack = "com.crvelox.launchchild"
-                            val action = "com.crvelox.launchchild.ACTION_CUSTOM_ACTION"
-
-                            try {
-                                val launchIntent = Intent(action)
-                                launchIntent.setPackage(pack)
-                                launchIntent.putExtra("data", "launchIntent")
-                                startActivity(launchIntent)
-                            } catch (e: ActivityNotFoundException) {
-                                // Target app not found or doesn't support the custom action, handle the error or notify the user
-                            }
+                            val packageName = "com.crvelox.launchchild"
+                            val activityName = "MainActivity"
+                            val dataList = listOf(Pair("data", "dataTo"))
+                            this@MainActivity.startOuterActivity(packageName, activityName, dataList)
                         }) {
                             Text(text = "To Child")
-
                         }
                     }
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LaunchParentTheme {
-        Greeting("Android")
     }
 }
